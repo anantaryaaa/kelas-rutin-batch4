a447eb1bb4ee801a402addf8f5bad1cdd53da9c7
anantaryaaa <ananta.novandi@gmail.com>
2025-10-26 10:11:10 +0700

    sesion 1

---
+++ Session-1/Solidity-101/SimplePlant.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract SimplePlant {
+    // TODO 1: Deklarasikan 5 state variables
+    // Hint: string public plantName;
+    // Hint: uint256 public ...
+    // ... tulis 4 lainnya!
+
+
+
+
+    // TODO 2: Buat constructor
+    constructor() {
+        // Set nilai awal untuk semua variables
+        // Hint: plantName = "Rose";
+        // Hint: owner = msg.sender;
+
+
+    }
+
+    // TODO 3: Buat fungsi water()
+    // Hint: function water() public { ... }
+
+
+    // TODO 4: Buat fungsi getAge()
+    // Hint: function getAge() public view returns (uint256) { ... }
+    // Hint: return block.timestamp - plantedTime;
+
+
+}
